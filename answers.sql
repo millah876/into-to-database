b26dec1c1d225df372e8b9468aa62f8e12a1efcc
millah876 <millah1895@gmail.com>
2025-11-07 17:39:22 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,8 @@
+-- Assignment: Introduction to Databases
+-- Student: [Your Name]
+
+-- 1. Create a new database called 'answers' only if it doesn't exist
+CREATE DATABASE IF NOT EXISTS answers;
+
+-- 2. Drop (delete) the database called 'demo' only if it exists
+DROP DATABASE IF EXISTS demo;
